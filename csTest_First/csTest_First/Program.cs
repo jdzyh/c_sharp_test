6cdc1365a601b8c46d6e458f7f7266380caa49d8
jdzyh <dtzyhy@163.com>
2015-02-15 10:59:41 +0800

    edit
    0.1

---
+++ csTest_First/csTest_First/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*快捷键：
+** CTRL+F5 调试运行
+** CTRL+j  补全代码
+**
+*/
+
+namespace csTest_First
+{
+    class Program
+    {   enum Test {AAAA=1, BBBB, CCCC};
+
+        static void Main(string[] args)
+        {
+            Test t = Test.BBBB;
+            int x = (int)Test.BBBB;
+
+            Console.WriteLine("{0}, {1}", t, x);
+        }
+    }
+}
